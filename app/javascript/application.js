541a11e2990927e7ba0711cb466dcc8e87ccd3f2
cteles <caioo.teless@gmail.com>
2025-09-16 20:26:35 -0300

    feat basic navigation menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
 import "jquery"
 import "semantic-ui"
 import "@hotwired/turbo-rails"
 import "controllers"
 
-document.addEventListener('DOMContentLoaded', () => {
-  $('.ui.dropdown').dropdown();
-  // inicialize aqui outros componentes Semantic UI que usar, ex:
-  // $('.ui.modal').modal();
+document.addEventListener("turbo:load", () => {
+    $('.ui.menu').tab();
+    $('.ui.dropdown').dropdown();
+    $('.ui.checkbox').checkbox();
+    $('.ui.modal').modal();
 });
