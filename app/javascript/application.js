68c4fa70839a216e2fa37c737d81b38677db212f
cteles <caioo.teless@gmail.com>
2025-09-16 19:45:47 -0300

    feat semantic ui for frontend

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "jquery"
+import "semantic-ui"
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('.ui.dropdown').dropdown();
+  // inicialize aqui outros componentes Semantic UI que usar, ex:
+  // $('.ui.modal').modal();
+});
