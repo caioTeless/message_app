95cebf522fd6a82e6f75cea847a1b3adb12c24b8
cteles <caioo.teless@gmail.com>
2025-09-23 18:22:07 -0300

    feat: messages functionality

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,7 @@
 class ChatroomController < ApplicationController
   before_action :require_user
   def index
+    @message = Message.new
     @messages = Message.includes(:user).order(created_at: :asc).limit(50)
   end
 end
